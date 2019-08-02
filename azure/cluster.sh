28b200104df6650bdba5709b3afc630ed43f6676
Lee Cattarin <lee.cattarin@gmail.com>
2019-08-02 00:07:52 -0700

    Updated cluster creation

--- azure/cluster.sh
+++ azure/cluster.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-if [[ "$#" -ne 2 ]]; then
-    echo "USAGE: ./cluster.sh \$RG_NAME \$CLUSTER_NAME"
+if [[ "$#" -eq 0 ]]; then
+    echo "USAGE: ./cluster.sh [\$RG_NAME\] \$CLUSTER_NAME"
     exit 0
 fi
 
-az group create \
-    --resource-group $1 \
-    --location westus2 \
-    --verbose
+if [[ "$#" -eq 2 ]]; then
+    az group create \
+        --name $1
+    az configure --defaults group=$1
+    shift
+fi
+
+MONITORING_WORKSPACE=$(az resource show \
+    --resource-type=Microsoft.OperationalInsights/workspaces \
+    --name lecattar-loganalytics \
+    --query "id" --output tsv \
+    2>/dev/null)
+
+if [[ -z "$MONITORING_WORKSPACE" ]]; then
+    MONITORING_WORKSPACE=$(az resource create \
+        --resource-type=Microsoft.OperationalInsights/workspaces \
+        --name lecattar-loganalytics \
+        -p '{"sku":{ "Name": "Standalone"} }' \
+        --query "id" --output tsv)
+fi
 
 az aks create \
-    --resource-group $1 \
-    --name $2 \
-    --admin-username $(whoami) \
-    --kubernetes-version "1.13.5" \
-    --node-count 1 \
-    --verbose
+    --name $1 \
+    --nodepool-name linux --node-count 1 --node-vm-size "Standard_DS3_v2" \
+    --enable-vmss --enable-addons monitoring \
+    --workspace-resource-id $MONITORING_WORKSPACE \
+    --ssh-key-value "$HOME/.ssh/id_rsa.pub" \
+    --admin-username $(whoami) --kubernetes-version "1.14.3" \
+    --query "{ name: name, resourceGroup: resourceGroup }"
 
 az aks get-credentials \
-    --resource-group $1 \
-    --name $2
+    --name $1
