f2d3c2edcb7d2e450e5a99e34a2175430ff86a73
Lee Cattarin <lee.cattarin@gmail.com>
2019-10-14 10:31:15 -0700

    Add setup script for some tools

---
+++ setup/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    apt-transport-https \
+    lsb-release \
+    gnupg
+
+# Microsoft signing key
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor | \
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+# Kubernetes signing key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+## Sources
+# Azure CLI
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+# Functions Core Tools
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+# Kubectl
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    kubectl \
+    azure-cli \
+    azure-functions-core-tools
+
+# Binaries
+curl -sL https://run.linkerd.io/install | sh
+
+# Github projects
+
+# Kubernetes plugins
+
