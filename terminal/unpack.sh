4b1110af79cd3a9fd335a7ab5f94298a68d1542f
Lee Cattarin <lee.cattarin@gmail.com>
2020-03-06 14:21:03 -0800

    Add script to unpack all backup files from configs folder

---
+++ terminal/unpack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in .bashrc .bash_aliases .zshrc .gitconfig .vimrc
+do
+	cp $HOME/scripts/terminal/configs/$file $HOME
+done
+
+rm -rf $HOME/.oh-my-zsh/custom
+cp -r $HOME/scripts/terminal/configs/oh-my-zsh/custom $HOME/.oh-my-zsh
+
+cp $HOME/scripts/terminal/configs/winterm.json /c/Users/leeca/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/profiles.json
