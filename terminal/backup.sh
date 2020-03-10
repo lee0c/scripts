a5aca789b922fe3fb495cbcd07aeab7e69fbeb79
Lee Cattarin <lee.cattarin@gmail.com>
2020-03-10 13:50:17 -0700

    Switch back to using lecattar as profile

--- terminal/backup.sh
+++ terminal/backup.sh
@@ -9,5 +9,5 @@ done
 
 cp -r $HOME/.oh-my-zsh/custom $HOME/scripts/terminal/configs/oh-my-zsh
 
-cp /c/Users/le*/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/profiles.json $HOME/scripts/terminal/configs/winterm.json
-cp /c/Users/le*/AppData/Roaming/Code/User/settings.json $HOME/scripts/terminal/configs/vscode_user.json
+cp /c/Users/lecattar/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/profiles.json $HOME/scripts/terminal/configs/winterm.json
+cp /c/Users/lecattar/AppData/Roaming/Code/User/settings.json $HOME/scripts/terminal/configs/vscode_user.json
