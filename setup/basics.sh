83ca130ad521e98a5ea37e621ee33470193b9320
Lee Cattarin <lecattar@microsoft.com>
2022-09-07 21:31:32 -0700

    update to match reorg path

--- setup/basics.sh
+++ setup/basics.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 if [[ ! -f /etc/wsl.conf ]]; then
         echo "Using wsl.conf to set mount point for c drive. Restart the terminal before running any scripts dependent on Windows file system."
-        cp $HOME/scripts/terminal/configs/wsl.conf /etc/wsl.conf
+        cp $HOME/scripts/setup/configs/wsl.conf /etc/wsl.conf
         exit
 fi
 
 for file in .gitconfig .vimrc
 do
-	cp $HOME/scripts/terminal/configs/$file $HOME
+	cp $HOME/scripts/setup/configs/$file $HOME
 done
