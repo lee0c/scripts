383935387cb8e906424d33e1546679f6daa8aa90
Lee Cattarin <lecattar@microsoft.com>
2022-09-08 11:10:15 -0700

    creating wsl.conf needs sudo

--- setup/basics.sh
+++ setup/basics.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -f /etc/wsl.conf ]]; then
         echo "Using wsl.conf to set mount point for c drive. Restart the terminal before running any scripts dependent on Windows file system."
-        cp $HOME/scripts/setup/configs/wsl.conf /etc/wsl.conf
+        sudo cp $HOME/scripts/setup/configs/wsl.conf /etc/wsl.conf
         exit
 fi
 
