946632677c764ea235663f33594ea030666e21e9
Lee Cattarin <lecattar@microsoft.com>
2024-10-01 14:58:41 -0700

    update flag script to make it leetle

--- terminal/flag-motd.sh
+++ terminal/flag-motd.sh
@@ -11,20 +11,20 @@ n="\e[0m"
 o="\e[37m"
 
 # System information
-[ -r /etc/lsb-release ] && . /etc/lsb-release
+# [ -r /etc/lsb-release ] && . /etc/lsb-release
 
-if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
+# if [ -z "$DISTRIB_DESCRIPTION" ] && [ -x /usr/bin/lsb_release ]; then
         # Fall back to using the very slow lsb_release utility
-        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
-fi
+#        DISTRIB_DESCRIPTION=$(lsb_release -s -d)
+# fi
 
 # Update information
-if [[ $(/usr/lib/update-notifier/apt-check --human-readable 2> /dev/null) ]]
-then
-    readarray -t updates <<< $(/usr/lib/update-notifier/apt-check --human-readable)
-else
-    readarray -t updates <<< "No update information available"
-fi
+# if [[ $(/usr/lib/update-notifier/apt-check --human-readable 2> /dev/null) ]]
+# then
+#    readarray -t updates <<< $(/usr/lib/update-notifier/apt-check --human-readable)
+# else
+#    readarray -t updates <<< "No update information available"
+# fi
 
 # Begin design
 printf "%-b" "${o}O${n}"
@@ -32,20 +32,22 @@ printf "\n"
 
 printf "%-b" "${o}\\\\#${re}\\\\&q${n}"
 printf "%14s" ""
-printf "Welcome to %s (%s %s %s)" "$DISTRIB_DESCRIPTION" "$(uname -o)" "$(uname -r)" "$(uname -m)"
+printf "settings:"
+# printf "Welcome to %s (%s %s %s)" "$DISTRIB_DESCRIPTION" "$(uname -o)" "$(uname -r)" "$(uname -m)"
 printf "\n"
 
 printf "%-b" " ${o}#\\\\${or}@\\\\${re}*@aq.${n}"
+printf "%12s" ""
+printf "ctrl+,"
 printf "\n"
 
 printf "%-b" " ${o}\\\\#${ye}\\\\@&${or}V@@${re}*@,.${n}"
-printf "%17s" ""
-printf "${updates[0]}"
+# printf "${updates[0]}"
 printf "\n"
 
 printf "%-b" "  ${o}#\\\\${gr}@&@${ye}*&@@${or}*\\\\${re}*&\\\\oo,,.${n}"
-printf "%9s" ""
-printf "${updates[1]}"
+# printf "%9s" ""
+# printf "${updates[1]}"
 printf "\n"
 
 printf "%-b" "  ${o}\\\\#${bl}\\\\@&${gr}@@&@@,${ye}*\\\\&og,${or}*&\\\\.${n}"
@@ -53,13 +55,12 @@ printf "%10s" ""
 printf "\n"
 
 printf "%-b" "   ${o}#\\\\${pu}@,${bl}\\\\&@@&,${gr}*@@\\\\&ogg,${ye}*&\\\\.${n}"
-printf "%8s" ""
-printf "settings: ctrl+,"
+# printf "%8s" ""
 printf "\n"
 
 printf "%-b" "   ${o}\\\\#${n}  ${pu}*\\\\&@gg,${bl}\\\\@@%&og\\\\,${gr}*@@%${n}"
-printf "%7s" ""
-printf "quake mode: win+\`"
+# printf "%7s" ""
+# printf "quake mode: win+\`"
 printf "\n"
 
 printf "%-b" "    ${o}#\\\\${n}     ${pu}***^^^^\\\\@g,${bl}@@@@${pu},g${n}"
